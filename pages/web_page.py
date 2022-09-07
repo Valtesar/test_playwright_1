42faeafabb69884c1460bcdf2e7995ae7e98ac62
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-07 13:59:15 +0400

    added abstract methods

--- pages/web_page.py
+++ pages/web_page.py
@@ -1,25 +1,29 @@
 from playwright.async_api import Page
 import pytest
+from abc import abstractmethod
 
 
-@pytest.mark.asyncio_cooperative
 class WebPage(object):
     def __init__(self, page: Page, height: int = 1600, width: int = 900):
         self.page = page
         self.height = height
         self.width = width
 
-    async def reload(self):
-        await self.page.reload(wait_until='load')
+    @abstractmethod
+    def reload(self):
+        self.page.reload(wait_until='load')
 
-    async def close_page(self):
-        await self.page.close()
+    @abstractmethod
+    def close_page(self):
+        self.page.close()
 
-    async def go_to_page(self, url):
-        await self.page.goto(url, wait_until='load')
+    @abstractmethod
+    def go_to_page(self, url):
+        self.page.goto(url, wait_until='load')
 
-    async def change_tab(self, function):
+    @abstractmethod
+    def change_tab(self, function):
         with self.page.context.expect_page(function) as new_page:
-            await function()
+            function()
         new_page = new_page.value
         return new_page
