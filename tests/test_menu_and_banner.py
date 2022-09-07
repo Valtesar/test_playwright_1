8a99d8e65a7ab77524b4cf3afe115c066d83c9a4
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-07 13:59:55 +0400

    added abstract methods, remake on sync

--- tests/test_menu_and_banner.py
+++ tests/test_menu_and_banner.py
@@ -1,13 +1,15 @@
 import pytest
 import logging
 from time import sleep
-
+from random import randrange
 
 logger = logging.getLogger('tests')
 
-@pytest.mark.asyncio_cooperative
+
 class TestMenuAndBanner:
-    @pytest.mark.asyncio_cooperative
-    async def test_menu_and_banner(self, start_page):
-        await start_page.pick_menu_block(1)
+    def test_menu_and_banner(self, start_page):
+        assert start_page.animation_block()
+        start_page.pick_menu_block(randrange(4))
         sleep(5)
+        assert not start_page.animation_block()
+        sleep(4)
