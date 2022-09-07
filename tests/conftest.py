247243498f06dd769fbacf9bbffb3f3e188f8a79
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-07 13:58:58 +0400

    remake with sync methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,12 @@
+from playwright.sync_api import Page
 import pytest
-from playwright.async_api import Page
 from pages.main_page import MainBankPage
 
 
-@pytest.mark.asyncio_cooperative
 @pytest.fixture()
-async def start_page(page: Page):
+def start_page(page: Page):
     main_page = MainBankPage(page)
-    await main_page.go_to_page(main_page.url)
+    main_page.go_to_page(main_page.url)
     return main_page
 
 
