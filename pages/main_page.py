2dde772df934c72ca119f2090205ba10ec7e31e8
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-07 19:04:03 +0400

    add doc string and get text from buttons animation method

--- pages/main_page.py
+++ pages/main_page.py
@@ -28,10 +28,9 @@ class MainBankPage(WebPage, ABC):
         self.banner_block = BannerBlock(page)
 
     def pick_menu_block(self):
-        self.page.locator(f'//*[contains(@class, "a3sPfj") and @tabindex = {current_index}]').click()
+        """Метод нажимает на случайную кнопку меню"""
 
-    def get_block_info(self):
-        pass
+        self.page.locator(f'//*[contains(@class, "a3sPfj") and @tabindex = {current_index}]').click()
 
     def get_banner_info(self):
         pass
@@ -46,18 +45,37 @@ class AnimationBlock:
         self._animation_block_body_text = None
 
     def get_animation_status(self):
+        """Метод проверяет, что анимация заполнения шкалы в карусели видна на странице"""
         self._animation_block_visibility = self.page.locator('//div[@class= "h3L1Y9"]').is_visible()
         return self._animation_block_visibility
 
     def get_animation_turnover(self):
+        """Метод проверяет, что  в анимации при заполнении шкалы происходит переключение на другую кнопку.
+           Возвращает --> True если кнопка переключилась, --> False если кнопка осталася прежней"""
+
         attribute_before = self.page.locator('//div[@class= "e3L1Y9"]').get_attribute('style')
         with self.page.expect_request('https://mc.yandex.ru/watch/**'):
             pass
         attribute_after = self.page.locator('//div[@class= "e3L1Y9"]').get_attribute('style')
         return attribute_before != attribute_after
 
-    def get_block_text(self):
-        pass
+    def get_animation_block_text(self):
+        """Метод получает текст из кнопок"""
+
+        self._animation_block_header_text = self.page.locator('//p[contains(@class, "a1zii3 h1zii3")]')\
+            .all_text_contents()
+        self._animation_block_body_text = self.page.locator('//p[contains(@class, "a1zii3 gG2mwi")]')\
+            .all_text_contents()
+
+        if len(self._animation_block_header_text) == len(self._animation_block_body_text):
+
+            self._animation_block_header_text = [w.replace(u'\xa0', u' ') for w in self._animation_block_header_text]
+            self._animation_block_body_text = [w.replace(u'\xa0', u' ') for w in self._animation_block_body_text]
+
+            print(dict(zip(self._animation_block_header_text, self._animation_block_body_text)))
+            return True
+        else:
+            return False
 
 
 class BannerBlock:
@@ -69,6 +87,8 @@ class BannerBlock:
         self._banner_block_body_text = None
 
     def get_banner_status(self):
+        """Метод проверяет, что текущий баннер связанный с индексом кнопки отображается корректно"""
+
         attribute_hidden = self.page.locator(f'(//div[contains(@class, "c2XhKl b2XhKl")][{current_index + 1}])')\
             .get_attribute('aria-hidden')
 
