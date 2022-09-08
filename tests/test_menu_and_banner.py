07376514c6080f5f485f6134b358332bba5b15e9
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-08 17:42:11 +0400

    refactoring

--- tests/test_menu_and_banner.py
+++ tests/test_menu_and_banner.py
@@ -3,7 +3,7 @@
 class TestMenuAndBanner:
     def test_menu_and_banner(self, start_page):
         assert start_page.animation_block.get_animation_turnover()
-        start_page.pick_menu_block()
+        start_page.pick_middle_menu_block()
         assert start_page.animation_block.get_animation_status(False)
         assert start_page.banner_block.get_banner_status()
         start_page.reload()
