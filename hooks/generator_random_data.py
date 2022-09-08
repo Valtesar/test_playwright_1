20fcb97fa1c119ecc86a800e9dacb803c6d42563
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-08 17:03:10 +0400

    added func for generate random email

--- hooks/generator_random_data.py
+++ hooks/generator_random_data.py
@@ -8,16 +8,27 @@ class GenerateRandomData:
 
     @classmethod
     def get_random_fio(cls):
+
         fio = [''.join(choices(string.ascii_lowercase, k=randrange(2, 7))) for _ in range(3)]
+
         return fio
 
     @classmethod
     def get_random_mobile(cls):
+
         mobile = randrange(9_000_000_000, 9_999_999_999)
+
         return mobile
 
-    def get_random_email(self):
-        pass
+    @classmethod
+    def get_random_email(cls):
+
+        def rand_text():
+            return ''.join(choices(string.ascii_lowercase, k=randrange(4, 7)))
+
+        email = "{}@{}.{}".format(rand_text(), rand_text(), 'com')
+
+        return email
 
 
 if __name__ == '__main__':
@@ -25,3 +36,4 @@ if __name__ == '__main__':
     print(x.get_random_fio())
     print(GenerateRandomData.get_random_fio())
     print(GenerateRandomData.get_random_mobile())
+    print(GenerateRandomData.get_random_email())
