363d2a07697c91c7d8029f3bc4c26f27b668048c
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-08 16:10:36 +0400

    update get animation method for wait animation visibility

--- tests/test_menu_and_banner.py
+++ tests/test_menu_and_banner.py
@@ -6,11 +6,10 @@ class TestMenuAndBanner:
     def test_menu_and_banner(self, start_page):
         assert start_page.animation_block.get_animation_turnover()
         start_page.pick_menu_block()
-        assert not start_page.animation_block.get_animation_status()
+        assert start_page.animation_block.get_animation_status(False)
         assert start_page.banner_block.get_banner_status()
         start_page.reload()
-        sleep(3)
-        assert start_page.animation_block.get_animation_status()
+        assert start_page.animation_block.get_animation_status(True)
         assert start_page.animation_block.get_animation_block_text()
 
 
