3feda7ee34b89bfd48a37081bfa70887f00fecac
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-09 13:22:50 +0400

    added fixture for credit_cards_page.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page
 import pytest
 from pages.main_page import MainBankPage
+from pages.credit_cards_page import CreditCardsPage
 
 
 @pytest.fixture()
@@ -8,5 +9,11 @@ def start_page(page: Page):
     main_page = MainBankPage(page)
     main_page.go_to_page(main_page.url)
     return main_page
+
+
+@pytest.fixture()
+def cards_page(page: Page):
+    cr_cards_page = CreditCardsPage(page)
+    return cr_cards_page
 
 
