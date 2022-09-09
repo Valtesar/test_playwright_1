075e25e5e7c23caad5cdf7d9f06d6266da53ac6a
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-09 14:55:48 +0400

    update test

--- tests/test_credit_cards.py
+++ tests/test_credit_cards.py
@@ -3,4 +3,4 @@
 class TestCreditCardsPage:
     def test_credit_card_order(self, start_page, cards_page):
         start_page.pick_upper_menu_block('Карты', 'Целый год без %')
-        cards_page.get_100_days_card()
+        assert cards_page.get_email_validation()
