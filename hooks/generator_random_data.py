8e494751385fec15cc6041deb2e7362e3f4df6fd
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-09 13:22:35 +0400

    refactor code

--- hooks/generator_random_data.py
+++ hooks/generator_random_data.py
@@ -6,25 +6,18 @@ class GenerateRandomData:
 
     @classmethod
     def get_random_fio(cls):
-
         fio = [''.join(choices(string.ascii_lowercase, k=randrange(2, 7))) for _ in range(3)]
-
         return fio
 
     @classmethod
     def get_random_mobile(cls):
-
         mobile = randrange(9_000_000_000, 9_999_999_999)
-
         return mobile
 
     @classmethod
     def get_random_email(cls):
-
         def rand_text():
             return ''.join(choices(string.ascii_lowercase, k=randrange(4, 7)))
-
         email = "{}@{}.{}".format(rand_text(), rand_text(), 'com')
-
         return email
 
