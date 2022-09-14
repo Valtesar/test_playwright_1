ac89bf1379f1090801c57020ed09e08980640862
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-14 13:25:20 +0400

    added get animation visible time func for sb

--- pages/small_business_page.py
+++ pages/small_business_page.py
@@ -1,6 +1,6 @@
-from pages.web_page import WebPage
-from pages.main_page import MainBankPage
 from playwright.sync_api import Page, expect
+import time
+import numpy as np
 
 
 class MainSmallBusinessPage:
@@ -13,3 +13,30 @@ class MainSmallBusinessPage:
 class AnimationBlockSmallBusiness:
     def __init__(self, page: Page):
         self.page = page
+
+    def get_animation_visible_time_sb(self):
+        """Метод измеряет время отображения анимации на каждом блоке и в случае расхождения времени возвращает -> False,
+           если время примерно равно, то возвращает -> True."""
+
+        blocks = [1, 2, 3, 0]
+        times = []
+
+        if not self.page.locator('//div[contains(@class, "i3sPf")]').is_visible():
+            return False
+
+        for i in blocks:
+            while not self.page.locator(f'//div[contains(@class, "i3sPf") and @tabindex="{i}"]').is_visible():
+                pass
+            else:
+                n = time.process_time()
+                while self.page.locator(f'//div[contains(@class, "i3sPf") and @tabindex="{i}"]').is_visible():
+                    pass
+                else:
+                    elapsed_time = time.process_time() - n
+                    times.append(elapsed_time)
+        print(times)
+        diff = np.diff(times)
+        for t in diff:
+            if t >= 1:
+                return False
+        return True
