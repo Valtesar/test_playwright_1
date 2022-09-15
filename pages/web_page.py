bdb8ecf456b9cbe1c5b63acb53565172dff8f02d
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-15 17:04:13 +0400

    added abstractmethod in webpage file

--- pages/web_page.py
+++ pages/web_page.py
@@ -9,21 +9,21 @@ class WebPage(object):
         self.height = height
         self.width = width
 
-    @abstractmethod
     def reload(self):
         self.page.reload(wait_until='load')
 
-    @abstractmethod
     def close_page(self):
         self.page.close()
 
-    @abstractmethod
     def go_to_page(self, url):
         self.page.goto(url, wait_until='load')
 
-    @abstractmethod
     def change_tab(self, function):
         with self.page.context.expect_page(function) as new_page:
             function()
         new_page = new_page.value
         return new_page
+
+    @abstractmethod
+    def get_title_of_the_tab(self):
+        pass
