85c03d79ecafcf29c0427664fdb8fcf56bef062c
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-16 16:22:22 +0400

    fix typo in property

--- hooks/time_comparison.py
+++ hooks/time_comparison.py
@@ -1,5 +1,3 @@
-from pages.main_page import AnimationBlockMain
-from pages.small_business_page import AnimationBlockSmallBusiness
 
 
 class TimeComparison:
@@ -17,18 +15,16 @@ class TimeComparison:
 
         if self._main_page_time < self._small_business_page_time:
             dif1 = self._small_business_page_time - self._main_page_time
-            print(f'Значение времени на странице малого бизнесса и ИП больше на {dif1}, чем на главной странице.')
+            print(f'\nЗначение времени на странице малого бизнесса и ИП больше на {dif1}, чем на главной странице.')
 
         elif self._main_page_time > self._small_business_page_time:
             dif2 = self._main_page_time - self._small_business_page_time
-            print(f'Значение времени на главной странице больше на {dif2}, чем на странице малого бизнесса и ИП')
+            print(f'\nЗначение времени на главной странице больше на {dif2}, чем на странице малого бизнесса и ИП')
 
         else:
-            print('Значения времени на главной странице и странице малого бизнесса и ИП равно или неверно.')
+            print('\nЗначения времени на главной странице и странице малого бизнесса и ИП равно или неверно.')
 
     @staticmethod
-    def output_time_comparison():
-        small_business_animation_time = AnimationBlockSmallBusiness.animation_time
-        main_animation_time = AnimationBlockMain.animation_time
-        time = TimeComparison(main_animation_time, small_business_animation_time)
+    def output_time_comparison(val1, val2):
+        time = TimeComparison(val1, val2)
         time.comparison_main_and_business_page()
