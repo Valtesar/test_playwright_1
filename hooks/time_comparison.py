1a48bcad37342efbb9331da67ae1a235f9b06a9f
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-16 14:17:29 +0400

    added comparison main and business page method

--- hooks/time_comparison.py
+++ hooks/time_comparison.py
@@ -2,9 +2,19 @@ class TimeComparison:
     """Класс для работы с объектами времени работы анимации на страницах сайта"""
 
     def __init__(self, val1, val2):
-        self._val1 = val1
-        self._val2 = val2
+        self._main_page_time = val1
+        self._small_business_page_time = val2
 
-    def comparison(self):
+    def comparison_main_and_business_page(self):
         """Метод сверяет два значения типа float и возвращает форматированную строку с разницей двух значений"""
-        pass
+
+        if self._main_page_time < self._small_business_page_time:
+            dif1 = self._small_business_page_time - self._main_page_time
+            print(f'Значение времени на странице малого бизнесса и ИП больше на {dif1}, чем на главной странице.')
+
+        elif self._main_page_time > self._small_business_page_time:
+            dif2 = self._main_page_time - self._small_business_page_time
+            print(f'Значение времени на главной странице больше на {dif2}, чем на странице малого бизнесса и ИП')
+
+        else:
+            print('Значения времени на главной странице и странице малого бизнесса и ИП равно или неверно.')
