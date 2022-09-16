e9335c42e82e62b150de7859e9c6accb739f4609
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-16 14:45:42 +0400

    added static method for compare and output time

--- hooks/time_comparison.py
+++ hooks/time_comparison.py
@@ -1,7 +1,14 @@
+from pages.main_page import AnimationBlockMain
+from pages.small_business_page import AnimationBlockSmallBusiness
+
+
 class TimeComparison:
     """Класс для работы с объектами времени работы анимации на страницах сайта"""
 
     def __init__(self, val1, val2):
+        """val1 - main_page_time
+           val2 - small_business_page_time"""
+
         self._main_page_time = val1
         self._small_business_page_time = val2
 
@@ -18,3 +25,10 @@ class TimeComparison:
 
         else:
             print('Значения времени на главной странице и странице малого бизнесса и ИП равно или неверно.')
+
+    @staticmethod
+    def output_time_comparison():
+        small_business_animation_time = AnimationBlockSmallBusiness.animation_time
+        main_animation_time = AnimationBlockMain.animation_time
+        time = TimeComparison(main_animation_time, small_business_animation_time)
+        time.comparison_main_and_business_page()
