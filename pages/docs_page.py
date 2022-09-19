fe1ef0002c18e30854d364d05d366ec90fce8a00
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-19 18:05:07 +0400

    added get files sizes method for docs page

--- pages/docs_page.py
+++ pages/docs_page.py
@@ -8,3 +8,8 @@ class DocsPage:
     def get_active_button(self):
         """Возвращает название текущей нажатой кнопки"""
         pass
+
+    def get_files_sizes(self):
+        """Метод собирает значения размера файлов на странице в список"""
+        pass
+
