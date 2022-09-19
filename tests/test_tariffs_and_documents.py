ca1d6198aa50034d3c7bb53acdbbeabceaf2c7d7
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-19 18:01:16 +0400

    update test

--- tests/test_tariffs_and_documents.py
+++ tests/test_tariffs_and_documents.py
@@ -1,3 +1,8 @@
+from time import sleep
+
+
 class TestTariffsAndDocuments:
-    def test_files(self, docs_and_tariffs_page):
+    def test_files(self, start_page, docs_and_tariffs_page):
+        start_page.pick_lower_menu_block('Тарифы и документы')
+        sleep(3)
         pass
