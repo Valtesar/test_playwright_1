cc9ad8b294cd18fec5ac2c517f68305cc94ef21d
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-19 18:03:44 +0400

    added get active button method for docs page

--- pages/docs_page.py
+++ pages/docs_page.py
@@ -5,4 +5,6 @@ class DocsPage:
     def __init__(self, page: Page):
         self.page = page
 
-
+    def get_active_button(self):
+        """Возвращает название текущей нажатой кнопки"""
+        pass
