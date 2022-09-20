33c257dced088aee1040c2c30059514c69a67bdb
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-20 21:04:07 +0400

    added file size transformation for transform str in the int with converting kb to mb

---
+++ hooks/file_size_transformation.py
@@ -0,0 +1,14 @@
+class FileSizeTransformation:
+    @staticmethod
+    def from_kb_to_mb(values: list) -> list:
+        result = []
+
+        for value in values:
+            if 'Кб' in value:
+                value = float(value.replace(',', '.')[:-3]) / 1024
+                result.append(float('{:.2f}'.format(value)))
+            elif 'Мб' in value:
+                result.append(float(value.replace(',', '.')[:-3]))
+
+        return result
+
