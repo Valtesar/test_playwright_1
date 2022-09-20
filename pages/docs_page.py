090463fe2abc391d997a9efdec531ad9a649313b
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-20 21:13:48 +0400

    added get file sizes method and pick the biggest file method

--- pages/docs_page.py
+++ pages/docs_page.py
@@ -1,9 +1,11 @@
 from playwright.sync_api import Page, expect
+from hooks.file_size_transformation import FileSizeTransformation
 
 
 class DocsPage:
     def __init__(self, page: Page):
         self.page = page
+        self.file_sizes = []
 
     def get_active_button(self):
         """Метод проверяет нахождения активной кнопки на странице,
@@ -15,10 +17,13 @@ class DocsPage:
 
     def get_files_sizes(self):
         """Метод собирает значения размера файлов на странице в список"""
-        pass
+        self.file_sizes = self.page.locator('//p[@class="aqMVUC bqMVUC hqMVUC DqMVUC cqMVUC eG2mwi RG2mwi"]')\
+            .all_text_contents()
+        self.file_sizes = FileSizeTransformation.from_kb_to_mb(self.file_sizes)
 
-    def pick_biggest_file(self):
+    def pick_the_biggest_file(self):
         """Выбирает самый большой файл из списка"""
+        print('Самый большой файл находящийся на странице имеет размер:', max(self.file_sizes))
         pass
 
     def try_download_file(self):
