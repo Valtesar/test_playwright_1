7ebb9ad7975e7a144e33a9d32da007a400e87dd1
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-20 20:31:29 +0400

    update test

--- tests/test_tariffs_and_documents.py
+++ tests/test_tariffs_and_documents.py
@@ -5,4 +5,6 @@ class TestTariffsAndDocuments:
     def test_files(self, start_page, docs_and_tariffs_page):
         start_page.pick_lower_menu_block('Тарифы и документы')
         sleep(3)
+        assert docs_and_tariffs_page.get_active_button() == "Комплексное банковское обслуживание"
+        sleep(2)
         pass
