fedafd425e3748e4054f3c5d015347737d02fbdc
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-28 18:40:27 +0400

    update test

--- tests/test_tariffs_and_documents.py
+++ tests/test_tariffs_and_documents.py
@@ -8,14 +8,10 @@ class TestTariffsAndDocuments:
         """Тест проверки наличия файлов тарифов и документации на странице, а так же получение их размеров"""
 
         start_page.pick_lower_menu_block('Тарифы и документы')
-        sleep(3)
         assert docs_and_tariffs_page.get_active_button() == "Комплексное банковское обслуживание"
-        sleep(2)
         docs_and_tariffs_page.get_files_sizes()
         docs_and_tariffs_page.pick_the_biggest_file()
         assert docs_and_tariffs_page.try_open_file()
         docs_and_tariffs_page.switch_active_button('Комплексное банковское обслуживание')
-        sleep(2)
         docs_and_tariffs_page.output_information()
-        sleep(5)
 
